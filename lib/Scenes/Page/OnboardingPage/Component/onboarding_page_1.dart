5fa0acf887873e848a11792490366548398115ac
irfandigalih <60963651+irfandigalih@users.noreply.github.com>
2023-07-28 17:49:11 +0700

    Add onboarding component page 1

--- lib/Scenes/Page/OnboardingPage/Component/onboarding_page_1.dart
+++ lib/Scenes/Page/OnboardingPage/Component/onboarding_page_1.dart
@@ -16,10 +16,12 @@ class _OnboardingPage1State extends State<OnboardingPage1> {
     return SafeArea(
       child: Scaffold(
         body: Container(
-          padding: const EdgeInsets.symmetric(horizontal: 24),
+          padding: const EdgeInsets.fromLTRB(24, 80, 24, 114),
           child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              const Text('timeboxing'),
+              const Text('Time Boxing'),
               RichText(
                   text: TextSpan(
                       text: 'is a',
@@ -33,7 +35,43 @@ class _OnboardingPage1State extends State<OnboardingPage1> {
                           TimeBoxingFontWeight.regular,
                           TimeBoxingColors.neutralBlack()),
                     )
-                  ]))
+                  ])),
+              const SizedBox(
+                height: 25,
+              ),
+              const Text(
+                  'Organize tasks, improve productivity, and increase your focus,'),
+              const SizedBox(
+                height: 44,
+              ),
+              Center(
+                child: Image.network(
+                    width: 100,
+                    height: 100,
+                    fit: BoxFit.fill,
+                    'https://www.american-time.com/wp-content/uploads/2021/02/91-National-24V-12in-Black-Steel-Round-Flush-Mount.jpg'),
+              ),
+              const Spacer(),
+              const Center(
+                child: Text(
+                    textAlign: TextAlign.center,
+                    'Full access awaits! Please login for a personalized and comprehensive experience.'),
+              ),
+              SizedBox(
+                height: 30,
+              ),
+              Container(
+                padding: const EdgeInsets.fromLTRB(150, 4, 150, 4),
+                decoration: const BoxDecoration(
+                  borderRadius: BorderRadius.all(Radius.circular(8)),
+                  color: Colors.green,
+                ),
+                width: MediaQuery.of(context).size.width,
+                child: const Text(
+                  textAlign: TextAlign.center,
+                  'data',
+                ),
+              )
             ],
           ),
         ),
