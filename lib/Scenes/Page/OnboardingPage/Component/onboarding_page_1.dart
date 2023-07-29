3c94674c715775d0d33864af25b79fb5bdb03f3a
irfandigalih <60963651+irfandigalih@users.noreply.github.com>
2023-07-29 21:16:04 +0700

    Adjust Flexible

--- lib/Scenes/Page/OnboardingPage/Component/onboarding_page_1.dart
+++ lib/Scenes/Page/OnboardingPage/Component/onboarding_page_1.dart
@@ -19,74 +19,90 @@ class _OnboardingPage1State extends State<OnboardingPage1> {
           padding: const EdgeInsets.fromLTRB(24, 80, 24, 114),
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
-            mainAxisAlignment: MainAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
-              Text('Time Boxing',
-                  style: TimeBoxingTextStyle.headline1Plus(
-                    TimeBoxingFontWeight.bold,
-                    TimeBoxingColors.text90(TimeBoxingColorType.shade),
-                  )),
-              RichText(
-                  text: TextSpan(
-                      text: 'is a',
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text('Time Boxing',
                       style: TimeBoxingTextStyle.headline1Plus(
-                          TimeBoxingFontWeight.bold,
-                          TimeBoxingColors.text90(TimeBoxingColorType.shade)),
-                      children: <TextSpan>[
-                    TextSpan(
-                      text: " Daily Planner",
-                      style: TimeBoxingTextStyle.headline1Plus(
-                          TimeBoxingFontWeight.bold,
-                          TimeBoxingColors.primary40(
-                              TimeBoxingColorType.shade)),
-                    )
-                  ])),
-              const SizedBox(
-                height: 25,
-              ),
-              Text(
-                'Organize tasks, improve productivity, and increase your focus,',
-                style: TimeBoxingTextStyle.paragraph1(
-                    TimeBoxingFontWeight.regular,
-                    TimeBoxingColors.text90(TimeBoxingColorType.shade)),
-              ),
-              const SizedBox(
-                height: 44,
-              ),
-              Center(
-                child: Image.network(
-                    width: 100,
-                    height: 100,
-                    fit: BoxFit.fill,
-                    'https://www.american-time.com/wp-content/uploads/2021/02/91-National-24V-12in-Black-Steel-Round-Flush-Mount.jpg'),
-              ),
-              const Spacer(),
-              Center(
-                child: Text(
-                    textAlign: TextAlign.center,
+                        TimeBoxingFontWeight.bold,
+                        TimeBoxingColors.text90(TimeBoxingColorType.shade),
+                      )),
+                  RichText(
+                      text: TextSpan(
+                          text: 'is a',
+                          style: TimeBoxingTextStyle.headline1Plus(
+                              TimeBoxingFontWeight.bold,
+                              TimeBoxingColors.text90(
+                                  TimeBoxingColorType.shade)),
+                          children: <TextSpan>[
+                        TextSpan(
+                          text: " Daily Planner",
+                          style: TimeBoxingTextStyle.headline1Plus(
+                              TimeBoxingFontWeight.bold,
+                              TimeBoxingColors.primary40(
+                                  TimeBoxingColorType.shade)),
+                        )
+                      ])),
+                  const SizedBox(
+                    height: 30,
+                  ),
+                  Text(
+                    'Organize tasks, improve productivity, and increase your focus,',
                     style: TimeBoxingTextStyle.paragraph1(
                         TimeBoxingFontWeight.regular,
                         TimeBoxingColors.text90(TimeBoxingColorType.shade)),
-                    'Full access awaits! Please login for a personalized and comprehensive experience.'),
+                  ),
+                ],
               ),
-              const SizedBox(
-                height: 30,
+              const Flexible(
+                flex: 1,
+                child: SizedBox(),
               ),
-              Container(
-                padding: const EdgeInsets.fromLTRB(150, 4, 150, 4),
-                decoration: BoxDecoration(
-                  borderRadius: const BorderRadius.all(Radius.circular(8)),
-                  color: TimeBoxingColors.primary30(TimeBoxingColorType.shade),
+              Flexible(
+                flex: 2,
+                child: Center(
+                  child: Image.network(
+                      fit: BoxFit.fill,
+                      'https://www.american-time.com/wp-content/uploads/2021/02/91-National-24V-12in-Black-Steel-Round-Flush-Mount.jpg'),
                 ),
-                width: MediaQuery.of(context).size.width,
-                child: Text(
-                  textAlign: TextAlign.center,
-                  style: TimeBoxingTextStyle.headline4(
-                      TimeBoxingFontWeight.bold,
-                      TimeBoxingColors.neutralWhite()),
-                  'Get Started',
-                ),
-              )
+              ),
+              const Flexible(
+                flex: 4,
+                child: SizedBox(),
+              ),
+              Column(
+                children: [
+                  Center(
+                    child: Text(
+                        textAlign: TextAlign.center,
+                        style: TimeBoxingTextStyle.paragraph1(
+                            TimeBoxingFontWeight.regular,
+                            TimeBoxingColors.text90(TimeBoxingColorType.shade)),
+                        'Full access awaits! Please login for a personalized and comprehensive experience.'),
+                  ),
+                  const SizedBox(
+                    height: 30,
+                  ),
+                  Container(
+                    padding: const EdgeInsets.fromLTRB(150, 4, 150, 4),
+                    decoration: BoxDecoration(
+                      borderRadius: const BorderRadius.all(Radius.circular(8)),
+                      color:
+                          TimeBoxingColors.primary30(TimeBoxingColorType.shade),
+                    ),
+                    width: MediaQuery.of(context).size.width,
+                    child: Text(
+                      textAlign: TextAlign.center,
+                      style: TimeBoxingTextStyle.headline4(
+                          TimeBoxingFontWeight.bold,
+                          TimeBoxingColors.neutralWhite()),
+                      'Get Started',
+                    ),
+                  )
+                ],
+              ),
             ],
           ),
         ),
